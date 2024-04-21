b182ea8f6edb525f4a3ed038b7203ee90a4fe531
rubiksper <63715088+rubiksper@users.noreply.github.com>
2024-04-21 14:45:43 +0200

    Update App.js

--- src/frontend/App.js
+++ src/frontend/App.js
@@ -5,12 +5,12 @@ import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Home from "../frontend/Home";
 import SignIn from "../frontend/SignIn";
 import SignUp from "../frontend/SignUp";
-import House from "../frontend/House"; // Importez le composant House
-import GuestHouse from "../frontend/GuestHouse"; // Importez le composant House
-import Dashboard from '../frontend/Dashboard'; // Importez le composant Dashboard
-import AdminProposal from '../frontend/AdminProposal'; // Importez le composant Dashboard
-import GuestProposal from '../frontend/GuestProposal'; // Importez le composant Dashboard
-import AdminHouse from '../frontend/AdminHouse'; // Importez le composant Dashboard
+import House from "../frontend/House"; 
+import GuestHouse from "../frontend/GuestHouse"; 
+import Dashboard from '../frontend/Dashboard'; 
+import AdminProposal from '../frontend/AdminProposal'; 
+import UserProposal from '../frontend/UserProposal'; 
+import AdminHouse from '../frontend/AdminHouse'; 
 
 function App() {
   return (
@@ -21,10 +21,10 @@ function App() {
         <Route path="/signup" element={<SignUp />} />
         <Route path="/home" element={<House />} />
         <Route path="/guest" element={<GuestHouse />} />
-        <Route path="/dashboard" element={<Dashboard />} /> {/* Nouvelle route pour le dashboard */}
-        <Route path="/GuestProposal" element={<GuestProposal />} /> {/* Nouvelle route pour le dashboard */}
-        <Route path="/AdminProposal" element={<AdminProposal />} /> {/* Nouvelle route pour le dashboard */}
-        <Route path="/AdminHouse" element={<AdminHouse />} /> {/* Nouvelle route pour le dashboard */}
+        <Route path="/dashboard" element={<Dashboard />} /> 
+        <Route path="/UserProposal" element={<UserProposal />} /> 
+        <Route path="/AdminProposal" element={<AdminProposal />} /> 
+        <Route path="/AdminHouse" element={<AdminHouse />} /> 
  
       </Routes>
     </Router>
